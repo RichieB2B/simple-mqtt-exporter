27cd03644dda8552965445c57df61a84e8b536c5
Richard van den Berg <richard@vdberg.org>
2023-09-04 11:59:13 +0200

    Support JSON payload in MQTT topics

--- simple-mqtt-exporter.py
+++ simple-mqtt-exporter.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import time
+import json
 import paho.mqtt.client as mqtt
 import prometheus_client as prom
 import argparse
@@ -30,7 +31,6 @@ def on_message(client, userdata, msg):
   if msg.topic in config.mqtt_topics:
     try:
       payload = str(msg.payload.decode("utf-8","strict"))
-      value = float(payload)
       succes[t] += 1
       received_messages.labels(status='succes', topic=msg.topic).set(succes[t])
     except Exception as e:
@@ -39,7 +39,35 @@ def on_message(client, userdata, msg):
       received_messages.labels(status='error', topic=msg.topic).set(error[t])
       return
     data_received = True
-    gauges[msg.topic].set(value)
+    # When the config is a list, we need to use the JSON fields in the message
+    if isinstance(config.mqtt_topics[msg.topic], list):
+      try:
+        content = json.loads(payload)
+      except Exception as e:
+        print(f'{type(e)}: {str(e)} while decoding json topic {t}')
+        error[t] += 1
+        received_messages.labels(status='error', topic=msg.topic).set(error[t])
+        return
+      for item in config.mqtt_topics[msg.topic]:
+        field = item['field']
+        if field in content:
+          try:
+            value = float(content[field])
+          except Exception as e:
+            print(f'{type(e)}: {str(e)} while decoding topic {t} field {field}')
+            error[t] += 1
+            received_messages.labels(status='error', topic=msg.topic).set(error[t])
+            continue
+          gauges[msg.topic + ':' + field].set(value)
+    else:
+      try:
+        value = float(payload)
+      except Exception as e:
+        print(f'{type(e)}: {str(e)} while decoding topic {t} field {field}')
+        error[t] += 1
+        received_messages.labels(status='error', topic=msg.topic).set(error[t])
+        return
+      gauges[msg.topic].set(value)
     updated.set(time.time())
 
 def mqtt_init():
@@ -67,19 +95,28 @@ if __name__ == '__main__':
     succes[t] = 0
     error[t] = 0
     parts = t.split('/')
-    name = v.get('name')
-    if not name:
-      name = parts[-1:][0]
-    description = v.get('help')
-    if not description:
-      description = t
-    labels = v.get('labels', {})
-    labels['topic'] = t
-    if len(parts) > 0:
-      labels['sensor'] = parts[1]
-    if not name in parents:
-      parents[name] = prom.Gauge(name, description, labels.keys())
-    gauges[t] = parents[name].labels(**labels)
+    if isinstance(v, list):
+      items = v
+      sep = ':'
+    else:
+      items = [v]
+      sep = ''
+    for i in items:
+      field = i.get('field', '')
+      topic = t + sep + field
+      name = i.get('name')
+      if not name:
+        name = parts[-1:][0]
+      description = i.get('help')
+      if not description:
+        description = t + sep + field
+      labels = i.get('labels', {})
+      labels['topic'] = t
+      if len(parts) > 0:
+        labels['sensor'] = parts[1]
+      if not name in parents:
+        parents[name] = prom.Gauge(name, description, labels.keys())
+      gauges[topic] = parents[name].labels(**labels)
   up = prom.Gauge('up', 'client status')
   updated = prom.Gauge('updated', 'data last updated in epoch')
   received_messages = prom.Gauge('received_messages', 'received messages per topic and status', ['status','topic'])
